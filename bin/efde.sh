0a204a889a867440893a5fd09aa2ff7561d5c933
Marucci Maximo <marucci.maximo@gmail.com>
2024-01-06 19:54:35 -0300

    feat (symfony): #15 migration symfony setup environment
    - Efde.Tasks
      - Generic element to manage the installation of any environment
    - Symfony.Tasks
      - New projects
      - Cloning of existing project
      - PHP, Symfony, Webapp version selection
    - Docker.Tasks: basic service management
      - General common task settings
      - Add common.tasks.command_line
      - Add common.tasks.env_variable
      - Fix core
      - Fix common.tasks.menu
      - Fix common.tasks.messages
      - Fix generals

--- bin/efde.sh
+++ bin/efde.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 # Debug mode
-EFDE_MOD_DEBUG=true # Messages debug
+EFDE_MOD_DEBUG=false # Messages debug
 EFDE_MOD_DEV=true # MMTodo: Prepared for create tmps
+EFDE_CFG_SHOW_CLI=true #show COMMANDLINE
+declare -gA GLOBAL_EFDE_CONFIG=(
+  [CLI_SHOW_DEFAULT]=true   # Omite configuraciones personalizadas
+  [CLI_SHOW_CLI]=true        #
+  [CLI_SHOW_OUTPUT]=false
+)
+
+
+GLOBAL_RUN_EFDE=$([ "$EFDE_MOD_DEBUG" = "true" ] && echo "e2" || echo "efde")
 
 [[ $(echo "$@" | grep -oP '(?<=--debug=)[^ ]+') = "true" && "$EFDE_MOD_DEBUG" = "false" ]] && EFDE_MOD_DEBUG=true
 
@@ -15,17 +24,15 @@ resolve_absolute_dir()
       SOURCE="$(readlink ${SOURCE})"
       [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
     done
+
     ABSOLUTE_BIN_PATH="$( cd -P "$( dirname ${SOURCE} )" && pwd )"
     ABSOLUTE_PATH="${ABSOLUTE_BIN_PATH}/.."
     EFDE_PATH_INSTALL="$(dirname $ABSOLUTE_BIN_PATH)"
 }
 
-
 init_dirs()
 {
     resolve_absolute_dir
-    export DOCKERGENTO_DIR="${ABSOLUTE_PATH}"
-
     export PATH_CONSOLE="${EFDE_PATH_INSTALL}/console"
     export PATH_BIN="${EFDE_PATH_INSTALL}/bin"
 
@@ -37,41 +44,26 @@ init_dirs()
 init_dirs
 source "$PATH_CONSOLE/init"
 
-
-
-main(){
-  if efde.tasks.implemention.has_folder_implementation ; then
-    #ES SYMFONY CARGA MENU
-    echo "symfony"
-  else
-    menu_init
-  fi
+menu_main(){
+  common.tasks.menu.print_menu "Select an option:" "${efde_props_menu_MAIN[@]}"
 }
 
-menu_init(){
+menu_implementation(){
+  local PATH_ENV=$(efde.tasks.implemention.get_current_path_env_file)
+  variables_string="$(efde.tasks.config.load_variables_from_file ${PATH_ENV})"
+  # Evaluar la cadena de variables
+  eval "$variables_string"
+  RUN_FUNCTION_IMPLEMENTATION_MAIN= "$IMPLEMENTION.tasks.menu.main"
 
-#IFS=',' read -ra elements <<< "${ALL_MENUS["MENU_INIT"]}"
-#
-#for element in "${elements[@]}"; do
-#  echo "$element"
-#done
-    #common.tasks.menu.print_menu "Select an option:" "${ALL_MENUS["MENU_INIT"][@]}"
-    common.tasks.menu.print_menu "Select an option:" "${MENU_INIT[@]}"
-    RESPONSE_CODE=${MENU_RESPONSE_ARRAY_ASOCITIVE['code']}
-    RESPONSE_TITLE=${MENU_RESPONSE_ARRAY_ASOCITIVE['title']}
+  ${RUN_FUNCTION_IMPLEMENTATION_MAIN}
+}
 
-    efde.tasks.implemention.setup_environment $RESPONSE_CODE
-    exit;
-    if common.tasks.module.exists_function "$RESPONSE_CODE.tasks.install.environment" ;then
-      efde.tasks.implemention.setup_environment $RESPONSE_CODE
-    else
-      common.tasks.message.danger "The installation of the selected environment is not yet available '$RESPONSE_TITLE'"
-
-      common.tasks.prompt.confirm_default_yes "Back to menu?" && menu_init
-    fi
+main(){
+  if ! efde.tasks.implemention.has_folder_implementation ; then
+    menu_main
+  fi
+  menu_implementation
 }
 
 main
-
-
 
